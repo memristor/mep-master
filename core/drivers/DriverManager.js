937d923a5c976513614aa147fddc526198476353
Darko Lukic <lukicdarkoo@gmail.com>
2016-09-09 20:14:01 +0200

    Improved documentation

--- core/drivers/DriverManager.js
+++ core/drivers/DriverManager.js
@@ -11,7 +11,16 @@ const TAG = 'DriverManager';
 var instance = null;
 
 /**
- * Manage drivers. Starts all drivers and provides instance of required driver
+ * <p>DriverManager's purpose is to manage drivers.</p>
+ *
+ * The main goals of Driver manager are:
+ * <ul>
+ *  <li>to choose which drivers to initialize for the big robot and the small robot,</li>
+ *  <li>to initialize drivers with correct parameters,</li>
+ *  <li>to choose between simulation and hardware implementation of driver,</li>
+ *  <li>to provide correct instance of driver,</li>
+ *  <li>to monitor if driver is active and to try to recover if it is not.</li>
+ * </ul>
  * @memberof drivers
  */
 class DriverManager {
@@ -30,7 +39,7 @@ class DriverManager {
     }
 
     /**
-     * Get instance of DriverManager. Don't `new` to get instance of DriverManager!
+     * Get instance of DriverManager. Do not use `new` to get instance of DriverManager!
      * @returns {DriverManager}
      */
     static get() {
@@ -41,9 +50,9 @@ class DriverManager {
     }
 
     /**
-     * Get service instance by service name
+     * Get driver instance by driver name
      *
-     * @param name {String} - Service name. Eg. `SCHEDULER`
+     * @param name {String} - Service name. Eg. `DriverManager.MOTION_DRIVER` or `DriverManager.MODBUS_DRIVER`
      * @returns {Object} - Required service
      */
     getDriver(name) {
