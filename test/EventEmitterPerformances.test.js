d3d013857f6830a924f69f746796bda13027802a
Darko Lukic <lukicdarkoo@gmail.com>
2016-10-01 21:33:15 +0200

    Added tests to inspect performances issues

---
+++ test/EventEmitterPerformances.test.js
@@ -0,0 +1,28 @@
+global.Mep = require('../src/Mep');
+const EventEmitter = require('events');
+
+const N = 50;
+
+class TestClass extends EventEmitter {
+    constructor() {
+        super();
+        this.setMaxListeners(N);
+    }
+
+    publish() {
+        this.emit('default');
+        return true;
+    }
+}
+
+describe('EventEmitterPerformances', function() {
+    var testObject = new TestClass();
+
+    for (let i = 0; i < N; i++) {
+        testObject.on('default', function () { });
+    }
+
+    it('should calculate publish time for ' + N + ' subscriptions', function() {
+        testObject.publish().should.equal(true);
+    });
+});
