bfeeeec3282cf32dafdc2d88b32a456c6cf2a34e
Darko Lukic <lukicdarkoo@gmail.com>
2016-10-19 22:24:40 +0200

    Add scheduler path verification during a bootstrap

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -1,5 +1,7 @@
 global.Mep = require('./Mep');
 
+const TAG = 'Bootstrap';
+
 class Bootstrap {
     constructor() {
         // Print status
@@ -7,12 +9,20 @@ class Bootstrap {
         Mep.Log.info('Active robot:', Mep.Config.get('robot'));
         Mep.Log.info('Active table:', Mep.Config.get('table'));
         Mep.Log.info('Use simulation:', Mep.Config.get('simulation'));
+        Mep.Log.info('Scheduler path:', Mep.Config.get('scheduler'));
 
         // Initialize DriverManager & services
         Mep.init();
 
         // Load strategy
-        const Scheduler = require(Mep.Config.get('scheduler'));
+        let Scheduler = null;
+        try {
+            Scheduler = require(Mep.Config.get('scheduler'));
+        } catch (e) {
+            let errorMessage = 'Scheduler at path ' + Mep.Config.get('scheduler') + ' cannot be found';
+            Mep.Log.error(TAG, errorMessage);
+            throw Error(errorMessage);
+        }
         let scheduler = new Scheduler();
         scheduler.runTask(scheduler.findBestTask());
     }
