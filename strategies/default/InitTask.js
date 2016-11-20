4e7ff8cc7faff80e4b090651c265f6f43119e169
Darko Lukic <lukicdarkoo@gmail.com>
2016-11-20 15:31:48 +0100

    Add constrains to codeclimate

--- strategies/default/InitTask.js
+++ strategies/default/InitTask.js
@@ -13,15 +13,24 @@ class InitTask extends Task {
     }
 
     onRun() {
+
         setTimeout(() => {
-            position.set(new TunedPoint(20, 0), {speed: 100})
-                .then(() => {
-                    return position.set(new TunedPoint(2370, 1130), { pathfinding: true })
-                })
-                .then(() => {
-                    return position.set(new TunedPoint(30, 30), { pathfinding: true })
-                });
+            if (true) {
+                position.set(new TunedPoint(100, -1200), {speed: 100})
+                    .then(() => {
+                        return position.set(new TunedPoint(50, -800), {pathfinding: true})
+                    })
+                    .then(() => {
+                        return position.set(new TunedPoint(400, -850), {pathfinding: true})
+                    });
+            } else {
+                //await position.set(new TunedPoint(20, 0), {speed: 100});
+                //await position.set(new TunedPoint(0, 0), { pathfinding: true });
+                //await position.set(new TunedPoint(30, 30), { pathfinding: true });
+            }
         }, 200);
+
+
 
         /*
         hand.open();
