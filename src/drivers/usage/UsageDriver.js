5bb9c76602a4b170b1c0a181eafb02af3967f608
Darko Lukic <lukicdarkoo@gmail.com>
2016-12-24 15:33:13 +0100

    Add UsageDriver to log cpu and memory usage

---
+++ src/drivers/usage/UsageDriver.js
@@ -0,0 +1,44 @@
+const Usage = require('usage');
+
+const TAG = 'UsageDriver';
+
+/**
+ * Logs memory and cpu usage
+ * @author Darko Lukic <lukicdarkoo@gmail.com>
+ * @example
+ * ...
+ * "UsageDriver": {
+ *     "class": "drivers/usage/UsageDriver",
+ *     "load": true,
+ *     "refreshPeriod": 1000
+ * }
+ * ...
+ */
+class UsageDriver {
+    constructor(name, config) {
+        this.name = name;
+        this.config = config;
+
+        this._usageLoop.bind(this);
+        this._usageLoop();
+    }
+
+    _usageLoop() {
+        let usageDriver = this;
+
+        setTimeout(() => {
+            Usage.lookup(process.pid, usageDriver._logUsage);
+            usageDriver._usageLoop();
+        }, this.config.refreshPeriod);
+    }
+
+    _logUsage(err, result) {
+        Mep.telemetry(TAG, result);
+    }
+
+    getGroups() {
+        return [];
+    }
+}
+
+module.exports = UsageDriver;
