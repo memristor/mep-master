4f3361f37491f36b753d14e8d7770fe1051010b7
Darko Lukic <lukicdarkoo@gmail.com>
2016-12-28 20:44:59 +0100

    Turn of telemetry by default

---
+++ tools/server/BrainServer.js
@@ -0,0 +1,20 @@
+const PORT = 1117;
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+    var address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(PORT);
