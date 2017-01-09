58eb26e06ddec1d12888f218eb7c932077337de4
Darko Lukic <lukicdarkoo@gmail.com>
2017-01-09 23:38:03 +0100

    Add default task error handling

--- src/types/Scheduler.js
+++ src/types/Scheduler.js
@@ -1,6 +1,24 @@
 const schedulerService = Mep.getSchedulerService();
 
+const TAG = 'Scheduler';
+
 class Scheduler {
+    constructor() {
+        process.on('unhandledRejection', this.onUnhandledTaskError.bind(this));
+    }
+
+    /**
+     * Run default action if there is the exception in task is not caught with `try {} catch(e) {}`.
+     * @param {TaskError} reason - Describes more about an exception
+     * @param p
+     */
+    onUnhandledTaskError(taskError, p) {
+        Mep.Log.warn(TAG, taskError);
+        Mep.Log.warn(TAG, 'Promise:', p);
+
+        this.runNextTask();
+    }
+
     getTasks() {
         return this.tasks;
     }
